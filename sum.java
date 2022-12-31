071fec2d98c555214b3b8c32f01cf5f982ba7d8c
Nirbhay Chaudhari <nirbhaychaudhari07@gmail.com>
2022-12-31 10:46:41 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+
+    public static void main(String args[]) {
+        int a = 32, b = 75, c;
+        c = a + b;
+        System.out.println("The sum of two numbers is:" + c);
+    }
+
+}
